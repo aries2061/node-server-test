864c04b866deb9604d3d3d6d1c71ae545a0958ee
aung <aries2061@gmail.com>
2023-04-30 15:06:14 +0630

    fixed

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res) => {
-    return res.status(200).json({"hello"});
+    const data = {
+        "Greeting": "Welcome"
+    };
+    return res.status(200).json({data});
 });
 
 const PORT = process.env.PORT || 3000;
