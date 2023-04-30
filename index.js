22aab05d5525a0aee0e5674fa50fbd80558465d0
aung <aries2061@gmail.com>
2023-04-30 15:00:42 +0630

    express

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const http = require("node:http");
+const express = require("express");
 
-const server = http.createServer((req,res) => {
-    res.writeHead(200, {"Content-Type":"text/plain"});
-    res.end("Hello");
+const app = express();
+
+app.get("/", (req,res) => {
+    return res.status(200).json({"hello"});
 });
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server is running on port 3000`));
+app.listen(PORT, () => console.log(`Server is running on port 3000`));
